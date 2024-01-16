4f9d6837cb6f02c431f7d422cdedf5c01f92489a
Tej Bari <136843098+tejbari5@users.noreply.github.com>
2024-01-16 10:08:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,71 @@
 function myRace(promises) {
-  // implementation
-}
-
-function myAny(promises) {
-  // implementation
-}
-
-function myAll(promises) {
-  // implementation
-}
-
-function myAllSettled(promises) {
-  // implementation
-}
-
-module.exports = {
-  myRace,
-  myAny,
-  myAll,
-  myAllSettled
-};
+    return new Promise((resolve, reject) => {
+      promises.forEach(promise => {
+        promise.then(resolve).catch(reject);
+      });
+    });
+  }
+  
+  function myAny(promises) {
+    return new Promise((resolve, reject) => {
+      let fulfilled = false;
+      promises.forEach(promise => {
+        promise.then(result => {
+          if (!fulfilled) {
+            fulfilled = true;
+            resolve(result);
+          }
+        }).catch(() => {
+          if (--count == 0) {
+            reject('all promises rejected');
+          }
+        });
+      });
+    });
+  }
+  
+  function myAll(promises) {
+    return new Promise((resolve, reject) => {
+      let results = [];
+      let count = promises.length;
+      promises.forEach((promise, index) => {
+        promise.then(result => {
+          results[index] = result;
+          if (--count == 0) {
+            resolve(results);
+          }
+        }).catch(reject);
+      });
+    });
+  }
+  
+  function myAllSettled(promises) {
+    return new Promise((resolve) => {
+      let results = [];
+      let count = promises.length;
+      promises.forEach((promise, index) => {
+        promise.then(result => {
+          results[index] = { status: 'fulfilled', value: result };
+          if (--count == 0) {
+            resolve(results);
+          }
+        }).catch(error => {
+          results[index] = { status: 'rejected', error: error };
+          if (--count == 0) {
+            resolve(results);
+          }
+        });
+      });
+    });
+  }
+  module.exports = {
+    myRace,
+    myAny,
+    myAll,
+    myAllSettled
+  };
+  
+  
+  
+  
+    
